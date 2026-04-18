7fc39fa5b36379ff83e1ae2117a206d05a6ecc60
HabibaAbdelfatah <habibaabdelfatah79@gmail.com>
2026-04-18 23:59:23 +0200

    CatCare-Models

---
+++ CatCare/Cat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatCare
+{
+    internal class Cat
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public HealthStatus HealthStatus { get; set; }
+        public List<HealthRecord> HealthRecords { get; set; }
+        public List<Schedule> Schedules { get; set; }
+
+        public Cat()
+        {
+            HealthRecords = new List<HealthRecord>();
+            Schedules = new List<Schedule>();
+        }
+        public string GetCareTip()
+        {
+            if (HealthStatus == HealthStatus.Sick)
+                return "Take your cat to the vet";
+
+            return "Your cat is healthy";
+        }
+    }
+}
---
+++ CatCare/OutdoorCat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatCare
+{
+    internal class OutdoorCat :Cat
+    {
+        public string OutdoorNote()
+        {
+            return "Be careful with outdoor risks";
+        }
+    }
+}
